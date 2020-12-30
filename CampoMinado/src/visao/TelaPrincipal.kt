6aebe279a446e25092a8286a95746f62eba632a6
Jeff Araujo <jefferson_lord@yahoo.com.br>
2020-12-30 17:08:10 -0300

    meu primeiro game com kotlin

---
+++ CampoMinado/src/visao/TelaPrincipal.kt
@@ -0,0 +1,42 @@
+package visao
+
+import modelo.Tabuleiro
+import modelo.TabuleiroEvento
+import javax.swing.JFrame
+import javax.swing.JOptionPane
+import javax.swing.SwingUtilities
+
+fun main(args: Array<String>) {
+    TelaPrincipal()
+}
+
+class TelaPrincipal : JFrame() {
+    private val tabuleiro = Tabuleiro(qtDeLinhas = 16, qtDeColunas = 30, qtDeMinas = 10)
+    private val painelTabuleiro = PainelTabuleiro(tabuleiro)
+
+    init {
+        tabuleiro.onEvento(this::mostrarResultado)
+        add(painelTabuleiro)
+
+        setSize(690, 438)
+        setLocationRelativeTo(null)
+        defaultCloseOperation = EXIT_ON_CLOSE
+        title = "Campo Minado"
+        isVisible = true
+    }
+
+    private fun mostrarResultado(evento: TabuleiroEvento) {
+        SwingUtilities.invokeLater {
+            val msg = when(evento) {
+                TabuleiroEvento.VITORIA -> "Você ganhou!"
+                TabuleiroEvento.DERROTA -> "Você perdeu!"
+            }
+
+            JOptionPane.showMessageDialog(this, msg)
+            tabuleiro.reiniciar()
+
+            painelTabuleiro.repaint()
+            painelTabuleiro.validate()
+        }
+    }
+}
